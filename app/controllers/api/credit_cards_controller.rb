8ecc26eedf2af932bbb218840cfc4370626ca033
Julia de L <jldelorimier@gmail.com>
2020-05-10 15:10:36 -0400

    Added concept of points to method: changed sign-on bonus to count points instead of dollars; added points back on dining, lyft, and \'everything else\'; added 15 \% off lyft

--- app/controllers/api/credit_cards_controller.rb
+++ app/controllers/api/credit_cards_controller.rb
@@ -38,15 +38,14 @@ class Api::CreditCardsController < ApplicationController
       spending_total_all_credit_cards_monthly: 2300,
       spending_amount_movable_monthly: true, 
       # spending_willing_to_change_credit_cards_monthly: "na", 
-      spending_lyft_total_monthly: 0,
-      #80 
+      spending_lyft_total_monthly: 80,
       personal_value_lyft_priority_airport_pickup_12mo: 60, 
       personal_value_lyft_cancel_ride_times_monthly: 0, 
       personal_value_lyft_lose_something_times_12mo: 1, 
       spending_meal_deliverly_total_monthly: 0, 
       spending_meal_deliverly_delivery_fee_monthly: 0, 
-      spending_travel_flights_next_12mo: 800, 
-      #4000
+      spending_travel_flights_next_12mo: 4000, 
+      #800
       spending_travel_hotels_next_12mo: 0, 
       personal_value_travel_airport_lounge_access_12mo: 80, 
       personal_value_concierge_service: 0, 
@@ -90,10 +89,21 @@ class Api::CreditCardsController < ApplicationController
     user_monthly_spending = answers[:spending_total_all_credit_cards_monthly]
     user_annual_spending = user_monthly_spending*12
     user_travel_spending_annual_simple = answers[:spending_lyft_total_monthly]*12 + answers[:spending_travel_flights_next_12mo] + answers[:spending_travel_hotels_next_12mo]
+    user_dining_monthly_spending = answers[:spending_dining_total_monthly]
+    user_dining_annual_spending = user_dining_monthly_spending*12
+    user_lyft_monthly_spending = answers[:spending_lyft_total_monthly]
+    user_lyft_annual_spending = user_lyft_monthly_spending*12
+    points = 0
     p "The user_monthly_spending variable is $#{user_monthly_spending}"
     p "The user_annual_spending variable is $#{user_annual_spending}"
     p "The user_travel_spending_annual_simple variable is $#{user_travel_spending_annual_simple}"
+    p "The user_dining_monthly_spending variable is $#{user_dining_monthly_spending}"
+    p "The user_dining_annual_spending variable is $#{user_dining_annual_spending}"
+    p "The user_lyft_monthly_spending variable is $#{user_lyft_monthly_spending}"
+    p "The user_lyft_annual_spending variable is $#{user_lyft_annual_spending}"
+    p "Points earned so far is 0."
 
+    # $300 ANNUAL TRAVEL CREDIT
     if user_travel_spending_annual_simple >= 300
       benefit += 300
       user_travel_spending_annual_simple -= 300
@@ -106,30 +116,66 @@ class Api::CreditCardsController < ApplicationController
     end
     p "The user_travel_spending_annual_simple variable is now $#{user_travel_spending_annual_simple}"
     p "The benefit to the user is now #{benefit}."
+
+    # 50,000 POINT SIGN-ON BONUS FOR FIRST YEAR
     if user_monthly_spending * 3 >= 4000
-      if user_travel_spending_annual_simple >= 750
-        benefit+=750
-        user_travel_spending_annual_simple -= 750
-        p "Adding $750 to benefits for travel credit; subtracting $750 from total annual travel spending for future benefit analysis."
-      elsif user_travel_spending_annual_simple < 750
-        p "Adding #{user_travel_spending_annual_simple} to benefit"
-        benefit += user_travel_spending_annual_simple
-        p "The benefit is now #{benefit}"
-        fiveHundredDividedByOnePointFive = user_travel_spending_annual_simple/1.5
-        p "fiveHundredDividedByOnePointFive = #{user_travel_spending_annual_simple/1.5}"
-        leftover_points_after_used_for_travel = card.sign_on_bonus_points - (user_travel_spending_annual_simple/1.5)*100
-        p "You have #{leftover_points_after_used_for_travel} points left from your sign-on bonus after using the rest of them on travel."
-        p "Subtracting $#{user_travel_spending_annual_simple} from the user_travel_spending_annual_simple variable."
-        user_travel_spending_annual_simple -= user_travel_spending_annual_simple
-        p "Adding $#{leftover_points_after_used_for_travel/100} to your benefit for cash back after using the rest of your sign-on bonus points towards travel."
-        benefit += leftover_points_after_used_for_travel/100
-      else
-      end
+      p "Adding 50000 points to the user for meeting the requirements of the sign-on bonus by spending over $4000 in 3 months."
+      points += 50000
+      #BEGINNING OF IF STATEMENT I COMMENTED OUT.
+      #Now I am thinking about it and realizing maybe I need to shape this method more in terms of points rather than dollars spent and each individual credit. 
+      # if user_travel_spending_annual_simple >= 750
+      #   benefit+=750
+      #   user_travel_spending_annual_simple -= 750
+      #   p "Adding $750 to benefits for travel credit; subtracting $750 from total annual travel spending for future benefit analysis."
+      # elsif user_travel_spending_annual_simple < 750
+      #   p "Adding #{user_travel_spending_annual_simple} to benefit"
+      #   benefit += user_travel_spending_annual_simple
+      #   p "The benefit is now #{benefit}"
+      #   fiveHundredDividedByOnePointFive = user_travel_spending_annual_simple/1.5
+      #   p "fiveHundredDividedByOnePointFive = #{user_travel_spending_annual_simple/1.5}"
+      #   leftover_points_after_used_for_travel = card.sign_on_bonus_points - (user_travel_spending_annual_simple/1.5)*100
+      #   p "You have #{leftover_points_after_used_for_travel} points left from your sign-on bonus after using the rest of them on travel."
+      #   p "Subtracting $#{user_travel_spending_annual_simple} from the user_travel_spending_annual_simple variable."
+      #   user_travel_spending_annual_simple -= user_travel_spending_annual_simple
+      #   p "Adding $#{leftover_points_after_used_for_travel/100} to your benefit for cash back after using the rest of your sign-on bonus points towards travel."
+      #   benefit += leftover_points_after_used_for_travel/100
+      # else
+      # end
+    #END OF THE IF STATEMENT I COMMENTED OUT
     else
       #you get no sign on bonus, so nothing happens.
     end
     p "The user_travel_spending_annual_simple variable is now $#{user_travel_spending_annual_simple}."
     p "The benefit to the user is now $#{benefit}."
+    p "The user now has #{points} points."
+
+    # 3% CASH BACK ON DINING
+    p "Adding #{user_dining_annual_spending *3} points to the user's points from annual spending on dining."
+    points += user_dining_annual_spending *3
+    p "The user now has #{points} points."
+
+    # 1% CASH BACK ON EVERYTHING ELSE
+    user_annual_spending_excluding_travel_and_dining_and_lyft = user_annual_spending - user_travel_spending_annual_simple - user_dining_annual_spending
+    p "The user spends $#{user_annual_spending_excluding_travel_and_dining_and_lyft} per year on credit cards excluding spending on travel, dining, and Lyft. This number is the value of the user_annual_spending_excluding_travel_and_dining_and_lyft variable."
+    p "Adding #{user_annual_spending_excluding_travel_and_dining_and_lyft} points to the user for this spending."
+    # p "I have #{points} points"
+    points += user_annual_spending_excluding_travel_and_dining_and_lyft 
+    p "The user now has #{points} points."
+
+    # SAVE 15% ON ALL LYFT:
+    savings_on_lyft_bc_15_percent_off = user_lyft_annual_spending * 0.15
+    p "The user will save $#{savings_on_lyft_bc_15_percent_off} on Lyft per year because they will get 15% off all Lyft rides and spend $#{user_lyft_annual_spending} on Lyft per year already. Adding $#{savings_on_lyft_bc_15_percent_off} to user benefit variable."
+    benefit += savings_on_lyft_bc_15_percent_off
+    p "User benefits now total $#{benefit}"
+    lyft_annual_spending_after_15_percent_off = user_lyft_annual_spending*0.85
+    p "After saving 15% off Lyft, the user will still spend $#{lyft_annual_spending_after_15_percent_off} on Lyft annually."
+
+    # 10 POINT BACK FOR EVERY DOLLAR SPENT ON LYFT
+    p "Adding #{lyft_annual_spending_after_15_percent_off * 10} points for the user from their annual Lyft spending (after accounting for 15% off)."
+    points += lyft_annual_spending_after_15_percent_off * 10
+    p "The user now has #{points} points."
+
+
 
 
 
